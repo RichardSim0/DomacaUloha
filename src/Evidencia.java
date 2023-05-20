0c354da8d82729dbaaedddd48f304b6cc540898f
Richard Šimo <richard.simo5@gmail.com>
2023-05-20 13:49:24 +0200

    Doplnenie D.Ú.

--- src/Evidencia.java
+++ src/Evidencia.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Evidencia {
+import java.time.LocalDate;
+
+public class Evidencia {
+
+    public static void main(String[] args) {
+        String name = "Jozef Mrkvička";
+        LocalDate dateOfBirth = LocalDate.of(2001,1,13);
+        int contracts = 24;
+        int carrot = 48;
+        String city = "Bratislava";
+        String SPZ = "BT235CA";
+        String fuelConsumption = "5.6l/100km";
+        String addressIP = "192.168.48.39";
+
+        double predaj = carrot / contracts;
+
+        System.out.println("Priemerné množstvo predanej mrkvy je: " + predaj + "ton");
+    }
 }
